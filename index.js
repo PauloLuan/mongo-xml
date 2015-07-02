5de9257ad6b0afa288d9d3fee5b3503095a4176d
Paulo Luan <pauloluan.inova@gmail.com>
2015-07-02 16:06:09 -0300

    added first version of saving files

--- index.js
+++ index.js
@@ -1,11 +1,88 @@
-var parser = require('xml2json');
-var fs = require('fs');
+'use strict';
+/**
+ * https://github.com/PauloLuan/
+ *
+ * Copyleft (c) 2015 Paulo Luan
+ * Licensed under the MIT license.
+ */
 
-fs.readFile('./NVTPOI_BRA_9500_001.xml', 'utf8', function(err, xml) {
-    if (err) {
-        return console.log(err);
+var parser = require('xml2json'),
+    Mongo = require('./mongo'),
+    assert = require('assert'),
+    Q = require('q'),
+    fs = require('fs'),
+    path = require('path'),
+    glob = require('glob'),
+    _ = require('lodash'),
+    colors = require('colors'),
+    self;
+
+module.exports = {
+
+    init: function() {
+        self = this;
+        var inputPath = "test/input";
+        var deferred = Q.defer();
+
+        Mongo.initialize_db(function() {
+            self.getAllXmlFiles(inputPath)
+                .then(function(files) {
+                    self.proccessFiles(files);
+                })
+                .then(function() {
+                    deferred.resolve();
+                })
+                .catch(function(error) {
+                    console.log(error);
+                    deferred.reject(error);
+                });
+        });
+
+        return deferred.promise;
+    },
+
+    proccessFiles: function(files) {
+        console.log(('Preparing ' + files.length + ' files to process.').yellow);
+
+        _.forEach(files, function(xmlPath, key) {
+            assert.equal(fs.existsSync(xmlPath), true, 'path should exist'.red);
+            console.log(('Processing: ' + xmlPath).yellow);
+            var xml = fs.readFileSync(xmlPath).toString('utf-8');
+            var content = parser.toJson(xml); //returns a string containing the JSON structure by default
+            //content = content.replace(/[^\w\s]/gi, '')
+            content = content.replace(/\$/g, "");
+            var json = JSON.parse(content);
+            var arrayToSave = json.DeliveryPackage.POI;
+
+            Mongo.save_entity(arrayToSave);
+            console.log((xmlPath + ' has been finished!').green);
+        });
+    },
+
+
+    /**
+     *
+     * Search recursivelly into a directory and returns all files that matches the searched file extension (.xml).
+     *
+     * @param destinationPath - the xml path
+     * @return {Promisse.<string[]>} files - the xml files.
+     * @rejects {ValidationError} if the input is too long
+     */
+    getAllXmlFiles: function(destinationPath) {
+        var deferred = Q.defer();
+
+        var destPath = path.join(destinationPath + '/**/*.xml');
+        glob(destPath, function(err, files) {
+            if (err) {
+                console.log("Error: ", err);
+                deferred.reject(err);
+            } else {
+                deferred.resolve(files);
+            }
+        });
+
+        return deferred.promise;
     }
+}
 
-    var json = parser.toJson(xml); //returns a string containing the JSON structure by default
-    console.log(json);
-});
+module.exports.init();
--- mongo.js
+++ mongo.js
@@ -24,9 +24,10 @@ function save_to_db(json, collection_name) {
     if (json) {
         // Connect to the db
         var collection = database.collection(collection_name);
+
         collection.insert(json, function(err, inserted) {
             if (err) {
-                //console.log(err);
+                console.log(err);
             } else {
                 console.log("salvo com sucesso!");
                 json = null;
@@ -40,7 +41,14 @@ function save_to_db(json, collection_name) {
 
 exports.save_entity = function(entity) {
     var collection_name = 'entities';
-    save_to_db(entity, collection_name);
+
+    if (Object.prototype.toString.call(entity) === '[object Array]') {
+        for (var i = entity.length - 1; i >= 0; i--) {
+            save_to_db(entity[i], collection_name);
+        };
+    } else {
+        save_to_db(entity, collection_name);
+    }
 };
 
 exports.delete_from_db = function(json, collection_name) {
@@ -62,82 +70,3 @@ exports.queryByObject = function(json, collection_name, callback) {
         collection = null;
     });
 };
-
-exports.export_to_json_file = function(json) {
-    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
-        console.log('File successfully written! - Check your project directory for the output.json file');
-    });
-};
-
-exports.exportToCSV = function() {
-    var processItem = function(entity_doc, done) {
-        appendTextToCsv(convertToCSV([entity_doc]), entity_doc, done);
-    };
-
-    var entities_collection = database.collection('entities');
-
-    entities_collection.find({}, function(err, result_cursor) {
-        mongoProcessing(result_cursor, processItem, 1, function(err) {
-            if (err) {
-                console.error('on noes, an error', err);
-                process.exit(1);
-            }
-        });
-    });
-};
-
-function convertToCSV(objArray) {
-    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-    var str = '';
-
-    for (var i = 0; i < array.length; i++) {
-        var line = '';
-        str += '\r\n';
-
-        for (var index in array[i]) {
-            if (line !== '') line += ',';
-
-            line += array[i][index];
-        }
-
-        str += line;
-    }
-
-    return str;
-}
-
-function appendTextToCsv(text, entity_doc, done) {
-    if (entity_doc.UF) {
-        var csv_path = 'output/' + entity_doc.UF + '.csv';
-
-        fs.exists(csv_path, function(exists) {
-            if (!exists) {
-                createColumnNames(csv_path, entity_doc, function() {
-                    appendText(csv_path, text, done);
-                });
-            } else {
-                appendText(csv_path, text, done);
-            }
-        });
-    } else {
-        done();
-    }
-}
-
-var appendText = function(csv_path, text, done) {
-    fs.appendFile(csv_path, text, function(err) {
-        if (err) {
-            console.log(err);
-        }
-
-        console.log(csv_path);
-
-        done();
-    });
-};
-
-function createColumnNames(csvPath, entity_doc, callback) {
-    var entity_keys = Object.keys(entity_doc);
-    var text = entity_keys;
-    return appendText(csvPath, text, callback);
-}
